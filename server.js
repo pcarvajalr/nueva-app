2d75b32e15b5d91c15a0034abda1d55e63ef40f1
Pedro Carvajal <galim_peiro96@hotmail.com>
2019-04-30 21:02:12 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/nueva-app'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/nueva-app/index.html'))
+});
+
+app.listen(process.env.PORT || 8080);
